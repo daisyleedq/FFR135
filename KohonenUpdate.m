8921a33c28a943a87e229c88857ed26fb4f50b4a
Erika Ek <erikaek@student.chalmers.se>
2017-09-29 17:08:38 +0200

    commented

--- KohonenUpdate.m
+++ KohonenUpdate.m
@@ -1,10 +1,21 @@
-function weights = KohonenUpdate(pattern, winningIndex, oldWeights, eta, sigma)
-    
+function weights = KohonenUpdate(pattern, winningIndex, weights, eta, sigma)
+%KohonenUpdate: performs a weight update according to Kohonen update rules
+%   
+%   INPUT AND OUTPUT:
+%   pattern - randomly selected pattern of size 1xN
+%   winningIndex - index of winning neuron
+%   weights - weights of size 1xN
+%   eta - eta parameter according to current phase
+%   sigma - sigma parameter according to current phase
+
+    % defines neighbouring function
     NeighbouringFunction = @(winInd, ind, sigma) exp(-abs((ind-winInd))^2/(2*sigma));
-    deltaWeights = zeros(size(oldWeights));
+    deltaWeights = zeros(size(weights));
     
-    for i = 1:size(oldWeights,1)
-        deltaWeights(i,:) = eta * (pattern-oldWeights(i,:)) * NeighbouringFunction(i,winningIndex,sigma);
+    % calculates weight difference for each pattern
+    for i = 1:size(weights,1)
+        deltaWeights(i,:) = eta * (pattern-weights(i,:)) * NeighbouringFunction(i,winningIndex,sigma);
     end
-    weights = oldWeights + deltaWeights;
+    % updates weights
+    weights = weights + deltaWeights;
 end
--- Problem1.m
+++ Problem1.m
@@ -1,5 +1,6 @@
 clear all, clc, clf, close all;
 
+% INITIALIZING VARIABLES
 M=100;
 N=1000;
 Torder=1e3;
@@ -7,62 +8,47 @@ Tconv=2e4;
 sigma0=100;
 eta0=0.1;
 tausigma=300;
-etaFunction=@(t)eta0*exp(-t/tausigma);
-sigmaFunction=@(t)sigma0*exp(-t/tausigma);
-
 input_data=zeros(N,2);
+weights = -1*ones(M, 2) +2*rand(M, 2);
 
+% GENEATING DATA
 n=0;
-figure(1),hold on
-plot([0 0],[0 1],'k');plot([0.5 0.5],[0 0.5],'k');plot([1 1],[0.5 1],'k');
-plot([0 0.5],[0 0],'k');plot([0.5 1],[0.5 0.5],'k');plot([0 1],[1 1],'k');
-figure(2)
-subplot(1,2,1)
-hold on
-plot([0 0],[0 1],'k');plot([0.5 0.5],[0 0.5],'k');plot([1 1],[0.5 1],'k');
-plot([0 0.5],[0 0],'k');plot([0.5 1],[0.5 0.5],'k');plot([0 1],[1 1],'k');
-subplot(1,2,2)
-hold on
-plot([0 0],[0 1],'k');plot([0.5 0.5],[0 0.5],'k');plot([1 1],[0.5 1],'k');
-plot([0 0.5],[0 0],'k');plot([0.5 1],[0.5 0.5],'k');plot([0 1],[1 1],'k');
-
 while n < N 
     x=rand;
     y=rand;
     if ~( x > 0.5 && y < 0.5 )
         n=n+1;
         input_data(n,:)=[x y];
-        figure(1)
-        plot(x,y,'o')
+        figure(1), hold on
+        plot(x,y,'o','LineWidth',1.2)
     end
 end
 
-weights = -1*ones(M, 2) +2*rand(M, 2);
-
-for t=1:(Torder+Tconv)
+% --- TRAINING NETWORK ---
+for t=1:(Torder+Tconv)% looping through all time steps
    
-    if t <= Torder
+    if t <= Torder % updating eta and sigma according in ordering phase
        
        eta=eta0*exp(-t/tausigma);
        sigma=sigma0*exp(-t/tausigma);
        
-       if t==Torder
-           figure(2)
-           subplot(1,2,1)
-           plot(weights(:,1),weights(:,2),'-o')
-           axis image
+       if t==Torder % plots the weights at the end of ordering phase
+           figure(2), hold on
+           plot(weights(:,1),weights(:,2),'-o','LineWidth',1.2)
        end
         
-    else
+    else % updating eta and sigma according in convergence phase
        
        eta=0.01;
        sigma=0.9;
         
     end
     
-    mu=randi([1 1000]);
+    mu=randi([1 1000]); % randomly selects updating pattern
     pattern=input_data(mu,:);
-    dist = 10^7;
+    
+    % derived index of winning neuron
+    dist = inf;
     winningIndex = 0;
     for i = 1:M
         tempDist = norm(pattern-weights(i,:));
@@ -72,16 +58,35 @@ for t=1:(Torder+Tconv)
         end
     end
     
+    % updates weights according to the Kohonen update rule
     weights=KohonenUpdate(pattern, winningIndex, weights, eta, sigma);
 
 end
 
+figure(3),hold on
+plot(weights(:,1),weights(:,2),'-o','LineWidth',1.2)
+
+% PLOTTING DETAILS
+figure(1)
+plot([0 0],[0 1],'k','LineWidth',2);plot([0.5 0.5],[0 0.5],'k','LineWidth',2);
+plot([1 1],[0.5 1],'k','LineWidth',2);plot([0 0.5],[0 0],'k','LineWidth',2);
+plot([0.5 1],[0.5 0.5],'k','LineWidth',2);plot([0 1],[1 1],'k','LineWidth',2);
+title('Original Data Set','Interpreter','latex','FontSize',18)
+xlabel('$\xi_1$','Interpreter','latex','FontSize',14)
+ylabel('$\xi_2$','Interpreter','latex','FontSize',14)
+set(gca,'FontSize',12)
 figure(2)
-subplot(1,2,2)
-plot(weights(:,1),weights(:,2),'-o')
-axis image
-
-
-
-
+plot([0 0],[0 1],'k','LineWidth',2);plot([0.5 0.5],[0 0.5],'k','LineWidth',2);
+plot([1 1],[0.5 1],'k','LineWidth',2);plot([0 0.5],[0 0],'k','LineWidth',2);
+plot([0.5 1],[0.5 0.5],'k','LineWidth',2);plot([0 1],[1 1],'k','LineWidth',2);
+title('Weight Vectors after Ordering Phase','Interpreter','latex','FontSize',18)
+xlabel('$w_1$','Interpreter','latex','FontSize',14)
+ylabel('$w_2$','Interpreter','latex','FontSize',14)
+figure(3)
+plot([0 0],[0 1],'k','LineWidth',2);plot([0.5 0.5],[0 0.5],'k','LineWidth',2);
+plot([1 1],[0.5 1],'k','LineWidth',2);plot([0 0.5],[0 0],'k','LineWidth',2);
+plot([0.5 1],[0.5 0.5],'k','LineWidth',2);plot([0 1],[1 1],'k','LineWidth',2);
+title('Weight Vectors after Convergence Phase','Interpreter','latex','FontSize',18)
+xlabel('$w_1$','Interpreter','latex','FontSize',14)
+ylabel('$w_2$','Interpreter','latex','FontSize',14)
 
